231c3c3e2e9d596e527dc12d9ca406b4102cda3e
AzarconAD <200471951+AzarconAD@users.noreply.github.com>
2025-06-23 23:58:53 +0800

    initialized enemy details

---
+++ enemies.py
@@ -0,0 +1,24 @@
+class Enemy:
+    def __init__(self, health=100, armor=20, basic_attack=10):
+        self.__health = health
+        self.__armor = armor
+        self.__basic_attack = basic_attack
+
+class Minion(Enemy):
+    def __init__(self):
+        super().__init__(health=100, armor=20, basic_attack=10)
+
+class Boss(Enemy):
+    def __init__(self):
+        super().__init__(health=100, armor=20, basic_attack=10)
+
+    def special_power(self):
+        self.enhanced_health = 50
+        self.__health += self.enhanced_health
+        self.enhanced_armor = 5
+        self.__armor += self.enhanced_armor
+        self.enhanced_basic_attack = 5
+        self.__basic_attack += self.enhanced_basic_attack
+
+
+
